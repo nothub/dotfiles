25292ad2942c58961fafad22938ed7161a50e42f
nothub <48992448+nothub@users.noreply.github.com>
2021-08-25 00:08:59 +0200

    port check

--- .bashrc.d/80-functions.bash
+++ .bashrc.d/80-functions.bash
@@ -10,6 +10,10 @@ export -f git-tagd
 function sshtunnel() { echo "digging tunnel from $1:$2 to localhost:$2" && ssh -L "$2":localhost:"$2" "$1"; }
 export -f sshtunnel
 
+# port open check
+function port-open() { timeout 1 bash -c "</dev/tcp/$1/$2" && echo "$1:$2 open" || echo "$1:$2 closed"; }
+export -f port-open
+
 # reset file perms
 function perms-reset-to-default() { find "$1" -type d -exec chmod 755 {} + && find "$1" -type f -exec chmod 644 {} +; }
 export -f perms-reset-to-default
