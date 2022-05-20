1c4612a2a3f538dc5cfa6ce77c9fc0a9605ebe8d
hub <48992448+nothub@users.noreply.github.com>
2022-05-20 09:15:19 +0200

    path additions

--- .bashrc.d/11-path.bash
+++ .bashrc.d/11-path.bash
@@ -1,15 +1,38 @@
+add_path() {
+  [[ ":$PATH:" != *":$1:"* ]] && PATH="$1:${PATH}"
+}
+
+# ~/.local/bin
 mkdir -p "$HOME/.local/bin/"
-if [ -d "$HOME/.local/bin" ]; then
-  [[ ":$PATH:" != *":${HOME}/.local/bin:"* ]] && PATH="${HOME}/.local/bin:${PATH}"
+add_path "${HOME}/.local/bin"
+
+# go
+if [[ -d "/usr/local/go/bin" ]]; then
+  add_path "/usr/local/go/bin"
+fi
+if [[ -d "$HOME/go/bin" ]]; then
+  add_path "$HOME/go/bin"
 fi
 
-if [ -d "/usr/local/go/bin" ]; then
-  [[ ":$PATH:" != *":/usr/local/go/bin:"* ]] && PATH="/usr/local/go/bin:${PATH}"
+# nim
+if [[ -d "$HOME/.nim-lang/bin" ]]; then
+  add_path "${HOME}/.nim-lang/bin"
 fi
 
-if [ -d "$HOME/.cargo/bin" ]; then
-  [[ ":$PATH:" != *":${HOME}/.cargo/bin:"* ]] && PATH="${HOME}/.cargo/bin:${PATH}"
+# zig
+if [[ -d "$HOME/.zig" ]]; then
+  add_path "${HOME}/.zig"
 fi
-if [ -f "$HOME/.cargo/env" ]; then
-  [[ ":$PATH:" != *":${HOME}/.cargo/env:"* ]] && PATH="${HOME}/.cargo/env:${PATH}"
+
+# platformio
+if [[ -d "$HOME/.platformio/penv/bin" ]]; then
+  add_path "${HOME}/.platformio/penv/bin"
+fi
+
+# rust
+if [[ -d "$HOME/.cargo/bin" ]]; then
+  add_path "${HOME}/.cargo/bin"
+fi
+if [[ -f "$HOME/.cargo/env" ]]; then
+  add_path "${HOME}/.cargo/env"
 fi
