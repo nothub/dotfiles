ce9d900202a5e4be2f30ee9e5d4c5bea83375416
hub <48992448+nothub@users.noreply.github.com>
2022-05-20 09:32:50 +0200

    ps1

--- .bashrc.d/98-prompt.bash
+++ .bashrc.d/98-prompt.bash
@@ -1,34 +1,40 @@
+if [[ -f /usr/share/git/git-prompt.sh ]]; then
+  source /usr/share/git/git-prompt.sh
+fi
+
 PROMPT_COMMAND=__prompt_command
 
 __prompt_command() {
 
-  local EXIT="$?"
+  local exit_code="$?"
 
-  local DEFAULT='\[\e[39m\]'
-  local     RED='\[\e[31m\]'
-  local  DIM_ON='\[\e[2m\]'
-  local DIM_OFF='\[\e[22m\]'
+  local FG_DEFAULT='\[\033[39m\]'
+  local FG_RED='\[\033[31m\]'
+  local DIM_ON='\[\033[2m\]'
+  local RESET='\[\033[0m\]'
 
-  PS1=""
-  PS1+="${DIM_ON}"
-  # user
-  PS1+="\u"
-  PS1+="@"
-  # host
-  PS1+="\h"
-  PS1+=":"
-  # dir
-  PS1+="\w"
-  # venv indicator
-  PS1+=$(if [ -z ${VIRTUAL_ENV+x} ]; then echo ""; else echo " (venv)"; fi)
-  # git branch
-  PS1+=$(__git_ps1)
-  PS1+="\n"
-  if [ $EXIT != 0 ]; then
-    PS1+="${RED}\$${DEFAULT} "
-  else
-    PS1+="\$ "
+  PS1="${DIM_ON}"
+
+  if [[ "$exit_code" != 0 ]]; then
+    PS1+="=> ${FG_RED}$exit_code${FG_DEFAULT}\n"
   fi
-  PS1+="${DIM_OFF}"
+
+  PS1+="\u@\h:\w"
+
+  # git branch
+  git_branch=$(__git_ps1 | sed s/[\(\)\ ]//g)
+  if [[ -n $git_branch ]]; then
+    if [[ $git_branch == *"main"* ]] || [[ $git_branch == *"master"* ]]; then
+      git_branch="${FG_RED}$git_branch${FG_DEFAULT}"
+    fi
+    PS1+=" ($git_branch)"
+  fi
+
+  # python venv
+  if [[ -n $VIRTUAL_ENV ]]; then
+    PS1+=" (venv)"
+  fi
+
+  PS1+="\n\$${RESET} "
 
 }
