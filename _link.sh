075ef41266c855116c41efdbc6deded280368573
hub <48992448+nothub@users.noreply.github.com>
2022-05-20 08:49:10 +0200

    dotfile linker

--- _link.sh
+++ _link.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
-mkdir -p "$HOME/.local/bin/"
-if [ ! -f "$HOME/.local/bin/reclink" ]; then
-  echo "installing reclink"
-  curl --progress-bar "https://raw.githubusercontent.com/nothub/reclink/master/reclink.py" -o "$HOME/.local/bin/reclink"
-  chmod +x "$HOME/.local/bin/reclink"
-fi
-
-"$HOME/.local/bin/reclink" --source . --target "$HOME" --replace --quiet --ignore ".git" ".gitignore" "_link.sh"
+.local/bin/reclink \
+  --source "$(dirname "$(readlink -f -- "$0")")" \
+  --target "$HOME" \
+  --replace \
+  --quiet \
+  --ignore ".git" ".gitignore" "_link.sh"
