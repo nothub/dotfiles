734ec4acfb04bec2dffa2b5686f6b4bd8bc241e9
hub <code@hub.lol>
2022-10-08 00:52:22 +0200

    golang, nix, nerdtree

--- .bashrc.d/60-tool-config.bash
+++ .bashrc.d/60-tool-config.bash
@@ -1,19 +1,44 @@
 # colors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 if [[ -x /usr/bin/dircolors ]]; then
-  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-  alias ls='ls --color=auto'
-  alias grep='grep --color=auto'
-  alias diff='diff --color=auto'
+    if [[ -d "${HOME}/.dircolors" ]]; then
+        eval "$(dircolors -b "${HOME}/.dircolors")"
+    else
+        eval "$(dircolors -b)"
+    fi
+    alias ls='ls --color=auto'
+    alias grep='grep --color=auto'
+    alias diff='diff --color=auto'
 fi
 
 # less non-text input files
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
-if [[ -d "$HOME/.sdkman" ]] && [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]]; then
-  export SDKMAN_DIR="$HOME/.sdkman"
-  source "$HOME/.sdkman/bin/sdkman-init.sh"
+# sdkman
+if [[ -r "${HOME}/.sdkman/bin/sdkman-init.sh" ]]; then
+    export SDKMAN_DIR="${HOME}/.sdkman"
+    source "${HOME}/.sdkman/bin/sdkman-init.sh"
 fi
 
-# kitty
-if test -n "$KITTY_INSTALLATION_DIR" -a -e "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; then source "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; fi
+# vim plugins
+if command -v vim >/dev/null 2>&1; then
+    if [[ ! -d ${HOME}/.vim/pack/vendor/start/nerdtree ]]; then
+        echo >&2 "installing nerdtree"
+        git clone https://github.com/preservim/nerdtree.git "${HOME}/.vim/pack/vendor/start/nerdtree"
+        vim -u NONE -c "helptags ${HOME}/.vim/pack/vendor/start/nerdtree/doc" -c q
+    fi
+fi
+
+# golang
+if [[ -d "${HOME}/go" ]]; then
+    export GOPATH="${HOME}/go"
+fi
+
+# nix
+if [[ -d ${HOME}/.nix-profile/etc/profile.d ]]; then
+    source "${HOME}/.nix-profile/etc/profile.d/nix-daemon.sh"
+    source "${HOME}/.nix-profile/etc/profile.d/nix.sh"
+elif [[ -d /nix/var/nix/profiles/default/etc/profile.d ]]; then
+    source "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh"
+    source "/nix/var/nix/profiles/default/etc/profile.d/nix.sh"
+fi
