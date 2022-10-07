a04fe4ef80e6e3dd22080e5bcf8f8e8487968aea
hub <code@hub.lol>
2022-10-08 00:51:24 +0200

    utility aliases

--- .bashrc.d/70-aliases.bash
+++ .bashrc.d/70-aliases.bash
@@ -11,6 +11,20 @@ alias clear-caches-java="rm -rf ~/.m2/repository/ ; rm -rf ~/.gradle/caches/"
 
 alias o="xdg-open"
 
+if command -v idea >/dev/null 2>&1; then
+    alias e="idea -e"
+elif command -v vim >/dev/null 2>&1; then
+    alias e="vim"
+elif command -v vi >/dev/null 2>&1; then
+    alias e="vi"
+elif command -v nano >/dev/null 2>&1; then
+    alias e="nano"
+elif command -v pico >/dev/null 2>&1; then
+    alias e="pico"
+else
+    alias e="xdg-open"
+fi
+
 alias ssh-pass="ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no"
 
 alias path='echo $PATH | sed -e "s/:/\n/g"'
@@ -20,14 +34,25 @@ alias cal="ncal -b -M"
 alias python="python3"
 alias pip="pip3"
 
-# kitty breaks stuff because of dumb politics otherwise
-alias ssh="TERM=xterm-256color ssh"
+alias ssh="TERM=xterm-256color ssh" # kitty breaks stuff because of dumb politics otherwise
+alias icat="kitty +kitten icat"
 
 if command -v git >/dev/null 2>&1; then
-  alias git-submodules-update="git pull --recurse-submodules && git submodule update --remote --recursive"
-  alias git-contribs="git log --all | sed -n 's/Author: //p' | sort -u"
+    alias git-submodules-update="git pull --recurse-submodules && git submodule update --remote --recursive"
+    alias git-contribs="git log --all | sed -n 's/Author: //p' | sort -u"
 fi
 
-if [[ -f ~/.bash_aliases ]]; then
-  . ~/.bash_aliases
+if [[ -f ${HOME}/.bash_aliases ]]; then
+    source "${HOME}/.bash_aliases"
 fi
+
+# sdkman
+alias sdk8="sdk-switch-java 8"
+alias sdk11="sdk-switch-java 11"
+alias sdk17="sdk-switch-java 17"
+
+alias nmap-scan="sudo nmap -T3 -F -O --traceroute"
+
+alias docker-jupyter='docker run --name jupyter -it --rm -p 8888:8888 jupyter/all-spark-notebook:latest'
+
+alias click-loop='while (true); do sleep 1; xdotool click 1; done'
