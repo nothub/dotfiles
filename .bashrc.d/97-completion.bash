b57d2f0bcd8154dc9b5dbddad4663a7a4806fd6d
hub <code@hub.lol>
2022-10-08 00:52:44 +0200

    more completions

--- .bashrc.d/97-completion.bash
+++ .bashrc.d/97-completion.bash
@@ -1,12 +1,35 @@
-if ! shopt -oq posix; then
-  if [[ -f /usr/share/bash-completion/bash_completion ]]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [[ -f /etc/bash_completion ]]; then
-    . /etc/bash_completion
-  fi
-fi
+# shellcheck source=/dev/null
+
 bind 'set completion-ignore-case on'
 
-if command -v pandoc >/dev/null 2>&1; then
-  eval "$(pandoc --bash-completion)"
+if shopt -oq posix; then return; fi
+
+if [[ -r /usr/share/bash-completion/bash_completion ]]; then
+    source /usr/share/bash-completion/bash_completion
+elif [[ -r /etc/bash_completion ]]; then
+    source /etc/bash_completion
+fi
+
+if command -v pandoc &>/dev/null; then
+    source <(pandoc --bash-completion)
+fi
+
+if command -v podman &>/dev/null; then
+    source <(podman completion bash)
+fi
+
+if command -v k3s &>/dev/null; then
+    source <(k3s completion bash)
+fi
+
+if command -v kubectl &>/dev/null; then
+    source <(kubectl completion bash)
+fi
+
+if command -v nerdctl &>/dev/null; then
+    source <(nerdctl completion bash)
+fi
+
+if command -v packwiz &>/dev/null; then
+    source <(packwiz completion bash)
 fi
