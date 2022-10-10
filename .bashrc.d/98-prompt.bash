c865a9cf6a195c52041ba14d6a289d9999399137
hub <code@hub.lol>
2022-10-10 16:12:16 +0200

    nix shell bash prompt info

--- .bashrc.d/98-prompt.bash
+++ .bashrc.d/98-prompt.bash
@@ -10,16 +10,28 @@ __prompt_command() {
 
     local FG_DEFAULT='\[\033[39m\]'
     local FG_RED='\[\033[31m\]'
+    local FG_CYAN='\[\033[36m\]'
+    local FG_YELLOW='\[\033[33m\]'
     local DIM_ON='\[\033[2m\]'
     local RESET='\[\033[0m\]'
 
     PS1="${DIM_ON}"
 
     if [[ "$exit_code" != 0 ]]; then
-        PS1+="=> ${FG_RED}$exit_code${FG_DEFAULT}\n"
+        PS1+="=> ${FG_RED}$exit_code${FG_RED}\n"
     fi
 
     PS1+="\u@\h:\w"
+
+    # nix shell
+    if [[ -n $IN_NIX_SHELL ]]; then
+        PS1+=" (${FG_CYAN}nix${FG_DEFAULT})"
+    fi
+
+    # python venv
+    if [[ -n $VIRTUAL_ENV ]]; then
+        PS1+=" (${FG_YELLOW}venv${FG_DEFAULT})"
+    fi
 
     # git branch
     local git_branch
@@ -28,12 +40,7 @@ __prompt_command() {
         if [[ $git_branch == *"main"* ]] || [[ $git_branch == *"master"* ]]; then
             git_branch="${FG_RED}$git_branch${FG_DEFAULT}"
         fi
-        PS1+=" ($git_branch)"
-    fi
-
-    # python venv
-    if [[ -n $VIRTUAL_ENV ]]; then
-        PS1+=" (venv)"
+        PS1+=" (git:$git_branch)"
     fi
 
     PS1+="\n\$${RESET} "
