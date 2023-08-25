1882592e866e691d75bfd610fb334c6d1439b196
Florian Hübner <code@hub.lol>
2023-08-25 22:57:45 +0200

    completions

--- .bashrc.d/97-completion.bash
+++ .bashrc.d/97-completion.bash
@@ -5,55 +5,63 @@ bind 'set completion-ignore-case on'
 if shopt -oq posix; then return; fi
 
 if [[ -r /usr/share/bash-completion/bash_completion ]]; then
-    source /usr/share/bash-completion/bash_completion
+  source /usr/share/bash-completion/bash_completion
 elif [[ -r /etc/bash_completion ]]; then
-    source /etc/bash_completion
+  source /etc/bash_completion
 fi
 
 should_add() {
-    if command -v "${1}" &>/dev/null && ! complete -p | grep "${1}" &>/dev/null; then
-        return 0
-    else
-        return 1
-    fi
+  if command -v "${1}" &>/dev/null && ! complete -p | grep "${1}" &>/dev/null; then
+    return 0
+  else
+    return 1
+  fi
 }
 
-if should_add pandoc; then
-    source <(pandoc --bash-completion)
+if should_add docker; then
+  source <(docker completion bash)
 fi
 
 if should_add podman; then
-    source <(podman completion bash)
-fi
-
-if should_add k3s; then
-    source <(k3s completion bash)
-fi
-
-if should_add k3d; then
-    source <(k3d completion bash)
-fi
-
-if should_add kubectl; then
-    source <(kubectl completion bash)
+  source <(podman completion bash)
 fi
 
 if should_add nerdctl; then
-    source <(nerdctl completion bash)
+  source <(nerdctl completion bash)
 fi
 
-if should_add packwiz; then
-    source <(packwiz completion bash)
+if should_add kubectl; then
+  source <(kubectl completion bash)
 fi
 
-if should_add gopass; then
-    source <(gopass completion bash)
+if should_add k3d; then
+  source <(k3d completion bash)
 fi
 
-if should_add hcloud; then
-    source <(hcloud completion bash)
+if should_add k3s; then
+  source <(k3s completion bash)
 fi
 
 if should_add terraform; then
   complete -C /usr/bin/terraform terraform
+fi
+
+if should_add packer; then
+  complete -C /usr/bin/packer packer
+fi
+
+if should_add hcloud; then
+  source <(hcloud completion bash)
+fi
+
+if should_add gopass; then
+  source <(gopass completion bash)
+fi
+
+if should_add pandoc; then
+  source <(pandoc --bash-completion)
+fi
+
+if should_add packwiz; then
+  source <(packwiz completion bash)
 fi
