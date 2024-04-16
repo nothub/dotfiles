73a9d2f2fb7ab2a9108eeb4eda1ecb48505093c9
Florian Hübner <fhuebner@posteo.de>
2024-04-16 19:28:07 +0200

    hook for mise

--- .bashrc.d/99-late-hooks.bash
+++ .bashrc.d/99-late-hooks.bash
@@ -1,4 +1,8 @@
 # direnv
-if command -v direnv >/dev/null 2>&1; then
+if command -v direnv > /dev/null 2>&1; then
     eval "$(direnv hook bash)"
+fi
+
+if command -v mise > /dev/null 2>&1; then
+    eval "$(mise activate bash)"
 fi
