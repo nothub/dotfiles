9a9dba85766799c77ee2b334f78601a1725151c9
Florian Hübner <fhuebner@posteo.de>
2024-04-16 19:20:40 +0200

    this might be destructive

--- .bashrc.d/80-functions.bash
+++ .bashrc.d/80-functions.bash
@@ -10,10 +10,6 @@ export -f git-tagd
 function sshtunnel() { echo "digging tunnel from $1:$2 to localhost:$2" && ssh -L "$2":localhost:"$2" "$1"; }
 export -f sshtunnel
 
-# reset file perms
-function perms-reset-to-default() { find "$1" -type d -exec chmod 755 {} + && find "$1" -type f -exec chmod 644 {} +; }
-export -f perms-reset-to-default
-
 # ddg
 function d() { ddgr --expand "$@"; }
 export -f d
