6f20ddb8b7fa18aeed52a87d5993f034fe7a959c
Florian Hübner <fhuebner@posteo.de>
2024-04-24 21:39:05 +0200

    stop loading nix profiles

--- .bashrc.d/62-nix.bash
+++
@@ -1,9 +0,0 @@
-if ! command -v devbox > /dev/null; then
-    if test -r "${HOME}/.nix-profile/etc/profile.d/nix.sh"; then
-        # shellcheck disable=SC1090,SC1091
-        source "${HOME}/.nix-profile/etc/profile.d/nix.sh"
-    elif test -r "/nix/var/nix/profiles/default/etc/profile.d/nix.sh"; then
-        # shellcheck disable=SC1090,SC1091
-        source "/nix/var/nix/profiles/default/etc/profile.d/nix.sh"
-    fi
-fi
