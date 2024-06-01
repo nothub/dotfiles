fb1865fba79d793570b4cac1073eb4fdbffa7864
Florian Hübner <fhuebner@posteo.de>
2024-06-01 18:38:21 +0200

    git utils

--- .bashrc.d/70-aliases.bash
+++ .bashrc.d/70-aliases.bash
@@ -36,6 +36,7 @@ alias icat="kitty +kitten icat"
 
 alias git-submodules-update="git pull --recurse-submodules && git submodule update --remote --recursive"
 alias git-contribs="git log --all | sed -n 's/Author: //p' | sort -u"
+alias git-hotstuff="git log --name-only --pretty=format: | grep -v '^\s*$' | sort | uniq -c | sort -nr"
 
 alias nmap-scan="sudo nmap -T3 -F -O --traceroute"
 
