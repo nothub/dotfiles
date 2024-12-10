b6b4099bd3ba7dcf71f5aa784a8f088442f09ab3
Florian Hübner <fhuebner@posteo.de>
2024-12-11 00:38:15 +0100

    smart goroot finder

--- .profile.d/20-go.sh
+++ .profile.d/20-go.sh
@@ -1,15 +1,34 @@
-if test -z "${GOROOT}"; then
+find_goroot() {
+
     if test -d "${HOME}/go"; then
         latest_version=$(ls "${HOME}/go" | grep -E "^go[0-9]+\.[0-9]+\.[0-9]+$" | sort -V | tail -n 1)
         if test -n "${latest_version}"; then
             export GOROOT="${HOME}/go/${latest_version}"
-        elif test -d "${HOME}/go/bin"; then
-            export GOROOT="${HOME}/go"
+            return
         fi
-    elif test -d "/usr/local/go"; then
-        export GOROOT="/usr/local/go"
     fi
+
+    if test -d "${HOME}/go/bin"; then
+        export GOROOT="${HOME}/go"
+        return
+    fi
+
+    if test -d "/usr/local/go/bin"; then
+        export GOROOT="/usr/local/go"
+        return
+    fi
+
+    if test -d "/usr/go/bin"; then
+        export GOROOT="/usr/go"
+        return
+    fi
+
+}
+
+if test -z "${GOROOT}"; then
+    find_goroot
 fi
+
 if test -n "${GOROOT}"; then
     add_path "${GOROOT}/bin"
 fi
