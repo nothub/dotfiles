e297f661da46488b74847722c6cdb1408660c304
Florian Hübner <fhuebner@posteo.de>
2025-01-19 17:51:44 +0100

    golang

--- .profile.d/20-go.sh
+++ .profile.d/20-go.sh
@@ -1,15 +1,7 @@
 find_goroot() {
 
-    if test -d "${HOME}/go"; then
-        latest_version=$(ls "${HOME}/go" | grep -E "^go[0-9]+\.[0-9]+\.[0-9]+$" | sort -V | tail -n 1)
-        if test -n "${latest_version}"; then
-            export GOROOT="${HOME}/go/${latest_version}"
-            return
-        fi
-    fi
-
-    if test -d "${HOME}/go/bin"; then
-        export GOROOT="${HOME}/go"
+    if test -d "${HOME}/.local/go/bin"; then
+        export GOROOT="${HOME}/.local/go"
         return
     fi
 
