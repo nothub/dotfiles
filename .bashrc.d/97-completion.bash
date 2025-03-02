70ca468e99e80bfab4a5c54ae9aece6d82a0e6b5
Florian Hübner <fhuebner@posteo.de>
2025-03-02 19:06:09 +0100

    properly escape kubectx & kubens completion

--- .bashrc.d/97-completion.bash
+++ .bashrc.d/97-completion.bash
@@ -53,7 +53,7 @@ if should_add kubectx; then
     _kube_contexts() {
         local curr_arg
         curr_arg=${COMP_WORDS[COMP_CWORD]}
-        COMPREPLY=($(compgen -W "- $(kubectl config get-contexts --output='name')" -- $curr_arg))
+        COMPREPLY=("$(compgen -W "- $(kubectl config get-contexts --output='name')" -- "${curr_arg}")")
     }
     complete -F _kube_contexts kubectx
 fi
@@ -62,7 +62,7 @@ if should_add kubens; then
     _kube_namespaces() {
         local curr_arg
         curr_arg=${COMP_WORDS[COMP_CWORD]}
-        COMPREPLY=($(compgen -W "- $(kubectl get namespaces -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}')" -- $curr_arg))
+        COMPREPLY=("$(compgen -W "- $(kubectl get namespaces -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}')" -- "${curr_arg}")")
     }
     complete -F _kube_namespaces kubens
 fi
