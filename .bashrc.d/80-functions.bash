5ccf786012ec99385df28ee8bd06b400fe1bfc64
Florian Hübner <fhuebner@posteo.de>
2025-10-28 20:02:37 +0100

    no exports needed
    none of these funcs should be consumed by any subsequently executed commands

--- .bashrc.d/80-functions.bash
+++ .bashrc.d/80-functions.bash
@@ -2,26 +2,21 @@
 
 # regex grep bash history
 function hse() { grep --text -E "$1" "${HOME}/.bash_history"; }
-export -f hse
 
 # git tag delete
 function git-tagd() { git tag -d "$1" && git push origin :refs/tags/"$1"; }
-export -f git-tagd
 
 # ssh port tunnel
 function sshtunnel() { echo "digging tunnel from $1:$2 to localhost:$2" && ssh -L "$2":localhost:"$2" "$1"; }
-export -f sshtunnel
 
 # ddg
 function d() { ddgr --expand "$@"; }
-export -f d
 
 # latest nixos packages hash
 function nix-pkg-hash() {
     echo >&2 "latest nixpkgs (nixos-unstable):"
     curl --fail --location --silent --show-error https://api.github.com/repos/NixOS/nixpkgs/commits/nixos-unstable | jq -r '.sha'
 }
-export -f nix-pkg-hash
 
 function topp() {
     local proc_infos
